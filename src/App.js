0e93343868eef11b09521446c4e60aa09b19f4a0
Ari Rexhaj <arirexhajj@gmail.com>
2024-06-19 16:34:00 +0200

    midgame finished?

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { useState } from "react";
 
 //how the game works:
 //rules for any gamestate:
-//1. if a player gets 3 bricks in a straight horizontal or vertical row, they make what is called a wall.
+//1. if a player gets 3 bricks in a straight horizontal or vertical line, they make what is called a wall.
 //   once a player has made a wall, they can remove one of the other players bricks from the board before their turn ends
 //2. a brick that is part of a wall is considered "walled" and cannot be removed from the board so long as it stays "walled"
 //3. if a brick that is part of a wall is moved such that the wall is no longer formed, it and the other bricks that made
@@ -22,7 +22,7 @@ import { useState } from "react";
 //a brick can only be moved one spot at a time, and moving a brick takes a turn.
 
 //endgame:
-//once a player only has 3 bricks left, they can move their bricks anywhere that is unoccupied on the map, this only applies to 
+//once a player only has 3 bricks left, they can move their bricks anywhere that is unoccupied on the map, this only applies to
 //the player with 3 bricks left, and can apply to both players if both only have 3 bricks left.
 
 //win condition: the first player to get the enemy players brick count to 2 wins. That is to say if you only have 2 bricks left, you lose
@@ -49,10 +49,10 @@ function generateMap(layers) {
   return map;
 }
 
-let wallList = [[],[]];
+let wallList = [[], []];
 function wallCheck(brickDiv, map) {
   let [layer, spot] = brickDiv.target.id.split("-");
-  let spotLoopList = [6,7,0,1,2,3,4,5,6,7,0]  //connects the list into circle
+  let spotLoopList = [6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0]; //connects the list into circle
 
   let foundNewWall = false;
   if (spot % 2 === 1) {
@@ -66,7 +66,7 @@ function wallCheck(brickDiv, map) {
       outerTeam === middleTeam &&
       middleTeam === innerTeam
     ) {
-      wallList[playing-1].push([
+      wallList[playing - 1].push([
         `0-${spot}-${map[0][spot]}`,
         `1-${spot}-${map[1][spot]}`,
         `2-${spot}-${map[2][spot]}`,
@@ -74,66 +74,78 @@ function wallCheck(brickDiv, map) {
       console.log("wall found 1", wallList);
       foundNewWall = true;
     }
-    spot = parseInt(spot) + 2
+    spot = parseInt(spot) + 2;
 
     //checking same dimensional walls
-    let leftTeam = map[layer][spotLoopList[(spot - 1)]];
+    let leftTeam = map[layer][spotLoopList[spot - 1]];
     let selfTeam = map[layer][spotLoopList[spot]];
-    let rightTeam = map[layer][spotLoopList[(spot + 1)]];
+    let rightTeam = map[layer][spotLoopList[spot + 1]];
 
     if (leftTeam !== 0 && leftTeam === selfTeam && selfTeam === rightTeam) {
-      wallList[playing-1].push([
-        `${layer}-${spotLoopList[spot - 1]}-${map[layer][spotLoopList[spot - 1]]}`,
+      wallList[playing - 1].push([
+        `${layer}-${spotLoopList[spot - 1]}-${
+          map[layer][spotLoopList[spot - 1]]
+        }`,
         `${layer}-${spotLoopList[spot]}-${map[layer][spotLoopList[spot]]}`,
-        `${layer}-${spotLoopList[spot + 1]}-${map[layer][spotLoopList[spot + 1]]}`,
+        `${layer}-${spotLoopList[spot + 1]}-${
+          map[layer][spotLoopList[spot + 1]]
+        }`,
       ]);
       console.log("wall found 2", wallList);
       foundNewWall = true;
-    } 
-  }
-  else {
-    spot = parseInt(spot) + 2 
-    let leftTeam1 = map[layer][spotLoopList[spot - 2]]
-    let middleTeam1 = map[layer][spotLoopList[spot - 1]]
-    let selfTeam = map[layer][spotLoopList[spot]]
-    let middleTeam2 = map[layer][spotLoopList[spot + 1]]
-    let leftTeam2 = map[layer][spotLoopList[spot + 2]]
+    }
+  } else {
+    spot = parseInt(spot) + 2;
+    let leftTeam1 = map[layer][spotLoopList[spot - 2]];
+    let middleTeam1 = map[layer][spotLoopList[spot - 1]];
+    let selfTeam = map[layer][spotLoopList[spot]];
+    let middleTeam2 = map[layer][spotLoopList[spot + 1]];
+    let leftTeam2 = map[layer][spotLoopList[spot + 2]];
 
     if (leftTeam1 !== 0 && leftTeam1 === selfTeam && selfTeam === middleTeam1) {
-      wallList[playing-1].push([
-        `${layer}-${spotLoopList[spot - 2]}-${map[layer][spotLoopList[spot - 2]]}`,
-        `${layer}-${spotLoopList[spot - 1]}-${map[layer][spotLoopList[spot - 1]]}`,
+      wallList[playing - 1].push([
+        `${layer}-${spotLoopList[spot - 2]}-${
+          map[layer][spotLoopList[spot - 2]]
+        }`,
+        `${layer}-${spotLoopList[spot - 1]}-${
+          map[layer][spotLoopList[spot - 1]]
+        }`,
         `${layer}-${spotLoopList[spot]}-${map[layer][spotLoopList[spot]]}`,
       ]);
       console.log("wall found 3", wallList);
       foundNewWall = true;
-    } 
+    }
 
     if (leftTeam2 !== 0 && leftTeam2 === selfTeam && selfTeam === middleTeam2) {
-      wallList[playing-1].push([
-        `${layer}-${spotLoopList[spot + 2]}-${map[layer][spotLoopList[spot + 2]]}`,
-        `${layer}-${spotLoopList[spot + 1]}-${map[layer][spotLoopList[spot + 1]]}`,
+      wallList[playing - 1].push([
+        `${layer}-${spotLoopList[spot + 2]}-${
+          map[layer][spotLoopList[spot + 2]]
+        }`,
+        `${layer}-${spotLoopList[spot + 1]}-${
+          map[layer][spotLoopList[spot + 1]]
+        }`,
         `${layer}-${spotLoopList[spot]}-${map[layer][spotLoopList[spot]]}`,
       ]);
       console.log("wall found 4", wallList);
       foundNewWall = true;
-    } 
-
+    }
   }
   return foundNewWall;
 }
 
-let gameState = 0;        //0 is pregame, 1 is midgame, 2 is endgame, 3 is game over
-let preGameRounds = 9*2;  //amount of turns before the pregame is finished/amount of bricks per team can place before pregame is over
-let playing = 1;          //what team is playing
+let gameState = 0; //0 is pregame, 1 is midgame, 2 is endgame, 3 is game over
+let preGameRounds = 9 * 2; //amount of turns before the pregame is finished/amount of bricks per team can place before pregame is over
+let playing = 1; //what team is playing
 let enemy = 2;
 let newWall = false;
-let brickList = [[],[]]
+let brickList = [[], []];
+
+let currentBrick = undefined
+let gotoBrick = undefined
 
 function App() {
   function gameMaster(brickDiv) {
     //controls the game
-
     function switchTurn() {
       if (playing === 1) {
         playing = 2;
@@ -143,88 +155,162 @@ function App() {
         enemy = 2;
       }
     }
+    if (newWall) {
+      console.log(`team${playing} is removing`);
+      //if a new wall is found:
+      if (
+        //check that the player isnt removing their own brick
+        brickDiv.target.id.split("-")[2] === playing.toString() ||
+        brickDiv.target.id.split("-")[2] === "0"
+      ) {
+        console.log("cannot remove your own brick");
+        return;
+      } else {
+        for (let wall of wallList[enemy - 1]) {
+          if (wall.includes(brickDiv.target.id)) {
+            console.log("cannot remove, is part of wall");
+            return;
+          }
+        }
+        //if the player can remove brick, the next click event will turn brick to team 0 (unoccupied)
+        console.log("removing brick", brickDiv.target.id);
+        for (let i = 0; i < brickList[enemy - 1].length; i++) {
+          if (brickList[enemy - 1][i] === brickDiv.target.id) {
+            brickList[enemy - 1].splice(i, 1);
+          }
+        }
+        updateMapSpot(brickDiv, "0");
+        newWall = false;
+        switchTurn();
+        return;
+      }
+    }
 
     if (gameState === 0) {
       //pregame
-      if (newWall) {
-        console.log(`team${playing} is removing`)
-        //if a new wall is found:
-        if ( //check that the player isnt removing their own brick
-          brickDiv.target.id.split("-")[2] === playing.toString() ||
-          brickDiv.target.id.split("-")[2] === "0"
-        ) {
-          console.log("cannot remove your own brick")
-          return;
+      console.log(`team${playing} is placing`);
+      if (brickDiv.target.id.split("-")[2] !== "0") {
+        console.log("spot is occupied, cannot place brick");
+        return;
+      }
+      let newMap = updateMapSpot(brickDiv, playing);
+      let [layer, spot] = brickDiv.target.id.split("-");
+      brickList[playing - 1].push(`${layer}-${spot}-${playing}`);
+      newWall = wallCheck(brickDiv, newMap);
+      preGameRounds -= 1;
+      if (preGameRounds === 0) {
+        //when all 18 moves have been made switch to midgame
+        gameState = 1;
+        console.log("pregame over");
+        if(!newWall) {
+          switchTurn()
         }
-        else {
-          for(let wall of wallList[enemy - 1]) {
-            if(wall.includes(brickDiv.target.id)) {
-              console.log("cannot remove, is part of wall")
-              return
-            }
-          }
-          //if the player can remove brick, the next click event will turn brick to team 0 (unoccupied)
-          console.log("removing brick",brickDiv.target.id);
-          for(let i = 0; i < brickList[enemy - 1].length; i++) {
-            if(brickList[enemy - 1][i] === brickDiv.target.id) {
-              brickList[enemy - 1].splice(i,1)
-            }
-          }
-          updateMapSpot(brickDiv, "0");
-          newWall = false;
-          switchTurn();
-          return;
-        }
-      } else {
-        console.log(`team${playing} is placing`)
-        if(brickDiv.target.id.split("-")[2] !== "0") {
+        return
+      }
+    }
+    
+    if(gameState === 1) { //midgame
+      if(currentBrick === undefined) {
+        if(brickDiv.target.id.split("-")[2] !== playing.toString()) {
+          console.log(`team${playing} must click a brick on your team`)
           return
         }
-        let newMap = updateMapSpot(brickDiv, playing);
-        let [layer,spot] = brickDiv.target.id.split("-")
-        brickList[playing-1].push(`${layer}-${spot}-${playing}`)
-        newWall = wallCheck(brickDiv, newMap);
-        preGameRounds -= 1;
-        if (preGameRounds === 0) {
-          //when all 18 moves have been made switch to midgame
-          gameState = 1;
-          console.log("pregame over")
+        currentBrick = brickDiv.target.id
+        console.log(`team${playing} is moving ${currentBrick}`);
+        return
+      }
+
+      if(brickDiv.target.id === currentBrick) {
+        currentBrick = undefined
+        console.log("cancelled move")
+        return
+      }
+      
+      if(brickDiv.target.id.split("-")[2] !== "0") {
+        console.log("spot is not unoccupied, cannot move there")
+        return
+      }
+
+      gotoBrick = brickDiv.target.id
+      let [moveSuccess,newMap] = moveBrick(currentBrick,gotoBrick)
+      if(moveSuccess) {
+
+        for(let i in wallList[playing-1]) {
+          if(wallList[playing-1][i].includes(currentBrick)) {
+            console.log("wall broken",wallList[playing-1][i])
+            wallList[playing-1].splice(i,1)
+          }
         }
 
+        newWall = wallCheck(brickDiv,newMap)
+        currentBrick = undefined
       }
-      if (!newWall) {
-        //when turn ends, switch turn
-        switchTurn();
+      else {
+        console.log("cannot move there, try again")
+        return
       }
-      else {  //if all enemy bricks are in walls, move on to next turn without removing any bricks
-        if(wallList[enemy-1].length === 0) {
-          return
-        } 
-        let clonedBrickList = [...brickList[enemy-1]]
-        for(let brick of brickList[enemy-1]) {
-          for(let wall of wallList[enemy-1]) {
-            if(wall.includes(brick)) {
-              console.log("brick:",brick,clonedBrickList)
-              for(let i = 0; i < clonedBrickList.length; i++) {
-                if(clonedBrickList[i] === brick) {
-                  clonedBrickList.splice(i,1)
-                }
+    }
+
+    if (!newWall) {
+      //when turn ends, switch turn
+      switchTurn();
+    } else {
+      //if all enemy bricks are in walls, move on to next turn without removing any bricks
+      if (wallList[enemy - 1].length === 0) {
+        return;
+      }
+      let clonedBrickList = [...brickList[enemy - 1]];
+      for (let brick of brickList[enemy - 1]) {
+        for (let wall of wallList[enemy - 1]) {
+          if (wall.includes(brick)) {
+            console.log("brick:", brick, clonedBrickList);
+            for (let i = 0; i < clonedBrickList.length; i++) {
+              if (clonedBrickList[i] === brick) {
+                clonedBrickList.splice(i, 1);
               }
             }
           }
         }
-        console.log(clonedBrickList)
-        if(clonedBrickList.length === 0) {
-          console.log("all enemy bricks are in walls, switching turns")
-          switchTurn()
-          newWall = false
-        }
+      }
+      console.log(clonedBrickList);
+      if (clonedBrickList.length === 0) {
+        console.log("all enemy bricks are in walls, switching turns");
+        switchTurn();
+        newWall = false;
       }
     }
   }
 
+  function moveBrick(current,goto) {
+    let [x1,y1,team] = current.split("-")
+    let [x2,y2] = goto.split("-")
+    let layerMoveLoop = [1,0,1,2,1]
+    let spotMoveLoop = [7,0,1,2,3,4,5,6,7,0]
+
+    let possibleMovements = []
+
+    if(parseInt(y1) % 2 === 1) {
+      possibleMovements.push(`${layerMoveLoop[parseInt(x1)+2]}-${spotMoveLoop[parseInt(y1)+1]}-0`)
+      possibleMovements.push(`${layerMoveLoop[parseInt(x1)]}-${spotMoveLoop[parseInt(y1)+1]}-0`)
+    }
+
+    possibleMovements.push(`${layerMoveLoop[parseInt(x1)+1]}-${spotMoveLoop[parseInt(y1)+2]}-0`)
+    possibleMovements.push(`${layerMoveLoop[parseInt(x1)+1]}-${spotMoveLoop[parseInt(y1)]}-0`)
+
+    if(possibleMovements.includes(goto)) {
+      let newGameMap = [...gameMap]
+      newGameMap[x1][y1] = 0
+      newGameMap[x2][y2] = parseInt(team)
+      setGameMap(newGameMap)
+      return [true,newGameMap]
+    }
+    else {
+      return [false,gameMap]
+    }
+  }
+
   function updateMapSpot(e, team) {
-    //used for pregame and mby endgame later
+    //used for pregame
     let [layer, spot] = e.target.id.split("-");
 
     let newGameMap = [...gameMap];
