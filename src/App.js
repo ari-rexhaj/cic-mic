a56ac1c327bef2915a55ba506ccb9435959c9540
Ari Rexhaj <arirexhajj@gmail.com>
2024-06-20 09:13:38 +0200

    updated readme

--- src/App.js
+++ src/App.js
@@ -5,28 +5,6 @@ import { useState } from "react";
 //1 => white team
 //2 => black team
 
-//how the game works:
-//rules for any gamestate:
-//1. if a player gets 3 bricks in a straight horizontal or vertical line, they make what is called a wall.
-//   once a player has made a wall, they can remove one of the other players bricks from the board before their turn ends
-//2. a brick that is part of a wall is considered "walled" and cannot be removed from the board so long as it stays "walled"
-//3. if a brick that is part of a wall is moved such that the wall is no longer formed, it and the other bricks that made
-//   up the wall are no longer "walled"
-//4. a brick can only be placed or moved on an unoccupied spot
-
-//pregame:
-//players take turn placing one of their 9 total reserve bricks onto the board
-
-//midgame:
-//players must now choose a brick to move and may only move it to a connected spot on the map (only moved to where the paths lead)
-//a brick can only be moved one spot at a time, and moving a brick takes a turn.
-
-//endgame:
-//once a player only has 3 bricks left, they can move their bricks anywhere that is unoccupied on the map, this only applies to
-//the player with 3 bricks left, and can apply to both players if both only have 3 bricks left.
-
-//win condition: the first player to get the enemy players brick count to 2 wins. That is to say if you only have 2 bricks left, you lose
-
 const spotSpots = [
   //fuck making an algorithm for this shit bruh, its the same for all layers
   ["0%", "0%"],
@@ -212,7 +190,6 @@ function App() {
     if(gameState === 1) { //midgame
       let endgame = false
       //endgame
-      console.log("team1",brickList[0],"team2",brickList[1])
       if(brickList[playing-1].length < 4) {
         endgame = true
         
